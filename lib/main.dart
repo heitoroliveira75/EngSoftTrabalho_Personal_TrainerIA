ca5b1b12dd373a036b51f9dc7b3a4b8da1bb1f36
arthur <arthur.peixoto@unifesp.br>
2026-09-21 01:52:54 -0300

    teste para transição de telas corrigido

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,19 @@ import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const FirstRoute());
+  runApp(const MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstRoute(), // Define a tela inicial aqui
+    );
+  }
 }
 
 class FirstRoute extends StatelessWidget {
